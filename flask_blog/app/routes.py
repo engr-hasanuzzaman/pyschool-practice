3039fa9737b8c67f6f2448c29da141bcaecaaadb
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-08-16 23:49:42 +0600

    Render form on login navigator

--- flask_blog/app/routes.py
+++ flask_blog/app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -11,3 +12,8 @@ def index():
     { 'title': '3rd post', 'des': '3rd post description' },
   ]
   return render_template('index.html', user = user, title = 'my blog', posts = posts)
+
+@app.route('/login')
+def login():
+  form = LoginForm()
+  return render_template('login.html', title='login', form=form)  
--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-  SECRETE_KEY = os.environ.get('secrete_key') or 'My supre secrete key'
+  SECRET_KEY = os.environ.get('secrete_key') or 'My supre secrete key'
   TEMPLATES_AUTO_RELOAD = True
