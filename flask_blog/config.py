fb879487e37dfce87bc3acf6e193bfd9ce25ff7a
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-08-18 22:26:25 +0600

    Create first migration files

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
   SECRET_KEY = os.environ.get('secrete_key') or 'My supre secrete key'
   TEMPLATES_AUTO_RELOAD = True
-  SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_UR') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_UR') or 'sqlite:///' + os.path.join(basedir, 'app.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   
