244e1cf0881a2f5800fe5d283c8548adb4540a65
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-08-18 22:07:54 +0600

    Create my fist model

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,5 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
   SECRET_KEY = os.environ.get('secrete_key') or 'My supre secrete key'
   TEMPLATES_AUTO_RELOAD = True
+  SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_UR') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  
