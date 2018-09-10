87ad577f85eeb8dd8788e9f455915e09c91a9e46
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-09-10 23:36:02 +0600

    Add mail server credential for sending error notification over email

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -8,3 +8,11 @@ class Config:
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   FLASK_DEBUG = True
   
+  # mail server credential
+  MAIL_SERVER = os.environ.get('MAIL_SERVER')
+  MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+  MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+  MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+  MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+  ADMINS = ['your-email@example.com']
+  
