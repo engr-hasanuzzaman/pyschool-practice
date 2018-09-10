c6eacfd7538fac8632d390f7e7deaaad35f9feb9
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-09-10 23:51:51 +0600

    add validtion to edit profile form

--- flask_blog/app/forms.py
+++ flask_blog/app/forms.py
@@ -30,3 +30,13 @@ class EditProifileForm(FlaskForm):
   about_me = StringField('About Me', validators=[Length(min=0, max=140)])
   username = StringField('Username', validators=[DataRequired()])
   submit = SubmitField('Submit')
+
+  def __init__(self, original_username, *args, **kwargs):
+        super(EditProifileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+  def validate_username(self, username):
+    if username.data != self.original_username:
+      user = User.query.filter_by(username=self.username.data).first()
+      if user is not None:
+        raise ValidationError('Please use a different username.')
