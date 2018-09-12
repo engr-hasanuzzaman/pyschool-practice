75e03cc5dba3a9b2ebaf6bf621ba917d0bd1b08b
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-09-12 22:28:01 +0600

    creat file function.py for storing function section code of pyschool

---
+++ Functions.py
@@ -0,0 +1,19 @@
+# 1: Add Two Numbers
+def addNumber(x, y):
+  return x + y
+
+# Subtract Numbers
+def subtractNumber(x, y): 
+  return x - y
+
+# Integer And Float
+# Change the type of the variable x to float
+# Change the type of variable y to integer
+x = 123446754336788543835697
+y = 3.14159265358979323846
+x = float(x)
+y = int(y)
+
+# Bigger Number
+def getBiggerNumber(n, m):
+  return n if n > m else m
