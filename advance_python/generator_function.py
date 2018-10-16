61854c116840a35b22477730d60e3d6a034f175f
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-10-16 11:12:33 +0600

    Practice python generator function

---
+++ advance_python/generator_function.py
@@ -0,0 +1,9 @@
+def simpleGeneratorFun(): 
+    yield 1            
+    yield 2            
+    yield 3
+
+for value in simpleGeneratorFun():  
+    print(value) 
+
+print(simpleGeneratorFun().next())   
