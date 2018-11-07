6dbc0f34b672e006f4ad3ace1212f24921007230
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-11-07 22:28:32 +0600

    add python cook

--- advance_python/python_way.py
+++ advance_python/python_way.py
@@ -9,3 +9,16 @@ for age in ages:
 
 for index, age in enumerate(ages):
   print("age is %d and index is %d" % (age, index))
+
+# swaping two variables
+## bad way
+a = 5
+b = 10
+temp = a
+a = b
+b = temp
+
+# python way
+a = 5
+b = 10
+a, b = b, a
