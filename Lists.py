e7357c43bb146849e873a079f482d73d92251e09
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-11-08 20:39:02 +0600

    add new file for list

---
+++ Lists.py
@@ -0,0 +1,3 @@
+# Create List
+## A list behaves like a container, and it is able to contain more than one value. Create a list with the values as shown in the example below.
+aList = ['Hello', 0, 20.0, 'World']
